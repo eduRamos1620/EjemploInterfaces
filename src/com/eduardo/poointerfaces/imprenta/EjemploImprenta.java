922fb66414bb04af26d576169480aa7c4eda2aa9
eduRamos1620 <eduardoramosrosales16@gmail.com>
2024-11-27 09:46:05 -0600

    add class Persona

--- src/com/eduardo/poointerfaces/imprenta/EjemploImprenta.java
+++ src/com/eduardo/poointerfaces/imprenta/EjemploImprenta.java
@@ -4,19 +4,21 @@ import com.eduardo.poointerfaces.imprenta.modelo.*;
 
 public class EjemploImprenta {
     public static void main(String[] args) {
-        Curriculum cv = new Curriculum("Juan", "Sistemas", "EStudiante de sistemas......");
+        Curriculum cv = new Curriculum(new Persona("Juan", "Perez"), "Sistemas",
+                "EStudiante de sistemas......");
         cv.addExperiencia("Java");
         cv.addExperiencia("Python");
         cv.addExperiencia("Spring");
         cv.addExperiencia("Django");
 
-        Libro libro = new Libro("Toño", "Los aliens existen", Genero.Suspenso);
+        Libro libro = new Libro(new Persona("Toño", "Sanchez"), "Los aliens existen", Genero.Suspenso);
         libro.addPagina(new Pagina("Sirenas"))
                 .addPagina(new Pagina("Pulpos"))
                 .addPagina(new Pagina("Animales extraños"))
                 .addPagina(new Pagina("Bombas nucleares???"));
 
-        Informe info = new Informe("Los extraterrestres vienen del oceano", "Juan", "Jaime Mausan");
+        Informe info = new Informe("Los extraterrestres vienen del oceano",
+                new Persona("Juan", "Perez"), new Persona("Jaime", "Mausan"));
 
         imprimir(cv);
         imprimir(info);
